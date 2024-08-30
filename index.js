56ec9afe3244e3eb090ece9d1600342be80318bb
HaroldHoang <viethunghoang1508@gmail.com>
2024-08-31 01:26:41 +0700

    Embed Bootstrap 4 and static file

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
-app.set("views", "./views")
-app.set("view engine", "pug")
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.use(express.static("public"));
 
 const route = require("./routes/clients/index.route");
 
