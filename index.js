9c5ddd1815ae42520383892ba9a803b9af297ccd
HaroldHoang <viethunghoang1508@gmail.com>
2024-08-31 00:25:10 +0700

    Import pug and config views folder

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set("views", "./views")
+app.set("view engine", "pug")
+
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    res.render("clients/pages/home/index");
+})
+
+app.get('/products', (req, res) => {
+    res.render("clients/pages/products/index");
 })
 
 app.listen(port, () => {
