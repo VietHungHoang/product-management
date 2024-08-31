9f49930e32cd130681a8e8b836fc703f9e9087d9
HaroldHoang <viethunghoang1508@gmail.com>
2024-08-31 10:03:36 +0700

    Connect database MongoDB

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const express = require('express');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
+const database = require("./config/database");
+const route = require("./routes/clients/index.route");
+
+database.connect();
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(express.static("public"));
 
-const route = require("./routes/clients/index.route");
-
 // Routes
 route(app);
+
 
 app.listen(port, () => {
     console.log(`Listen success on port ${port}`);
