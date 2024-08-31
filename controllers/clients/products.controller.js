24124067959b5a64f33793acd276ed991ae99617
HaroldHoang <viethunghoang1508@gmail.com>
2024-08-31 10:33:59 +0700

    Filter products

--- controllers/clients/products.controller.js
+++ controllers/clients/products.controller.js
@@ -2,7 +2,10 @@ const Product = require("../../models/product.model");
 
 module.exports.index = async (req, res) => {
 
-    const products = await Product.find({});
+    const products = await Product.find({
+        status: "active", 
+        deleted: false
+    });
 
     console.log(products);
 
