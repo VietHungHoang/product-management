6735507bdb3891a1355e76d5c428593525f1a562
HaroldHoang <viethunghoang1508@gmail.com>
2024-08-31 10:29:36 +0700

    Get all products from database

--- controllers/clients/products.controller.js
+++ controllers/clients/products.controller.js
@@ -1,4 +1,11 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+
+    const products = await Product.find({});
+
+    console.log(products);
+
     res.render("clients/pages/products/index",{
         pageTitle: "Products"
     });
