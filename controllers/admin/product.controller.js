b76dbba59fa9dc4acec143a2ed366c5f1ea3380d
HaroldHoang <viethunghoang1508@gmail.com>
2024-09-07 21:10:07 +0700

    filter products backend

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,11 +1,16 @@
 const Product = require("../../models/product.model");
 
 module.exports.index = async (req, res) => {
+    console.log(req.query.status);
 
-    const products = await Product.find({
+    let find = {
         deleted: false
-    });
-    console.log(products);
+    }
+
+    if(req.query.status) find.status = req.query.status;
+
+    const products = await Product.find(find);
+    // console.log(products);
 
     res.render("admin/pages/products/index", {
         products: products
