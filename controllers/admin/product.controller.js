987103955860752b7613a3521b8d1e4d2bb88852
HaroldHoang <viethunghoang1508@gmail.com>
2024-09-07 21:54:06 +0700

    Complete filter products

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,16 +1,49 @@
 const Product = require("../../models/product.model");
 
 module.exports.index = async (req, res) => {
+
+    let filterStatus = [
+        {
+            name: "Tất cả", 
+            status:"", 
+            class: ""
+        }, 
+        {
+            name: "Hoạt động", 
+            status: "active",
+            class: ""
+        },
+        {
+            name: "Dừng hoạt động", 
+            status: "inactive",
+            class: ""
+        }
+    ];
+
     let find = {
         deleted: false
     }
+    console.log(filterStatus);
 
-    if(req.query.status) find.status = req.query.status;
+    let queryStatus = req.query.status;
+
+    if(queryStatus){
+        const index = filterStatus.findIndex(item => item.status == queryStatus);
+        filterStatus[index].class = "active";
+    }
+    else{
+        const index = filterStatus.findIndex(item => item.status == "");
+        filterStatus[index].class = "active";
+    }
+    console.log(filterStatus);
+
+    if(queryStatus) find.status = queryStatus;
 
     const products = await Product.find(find);
     // console.log(products);
 
     res.render("admin/pages/products/index", {
-        products: products
+        products: products,
+        filterStatus: filterStatus
     });
 };
