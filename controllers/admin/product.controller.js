c1f7a8e584a369d588cabb42167c3dc2a6d23c03
HaroldHoang <viethunghoang1508@gmail.com>
2024-09-26 16:09:36 +0700

    delete permanently

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -40,7 +40,7 @@ module.exports.index = async (req, res) => {
     });
 };
 
-//[PATCH] /admin/products/change-status/status/id
+//[PATCH] /admin/products/change-status/:status/:id
 module.exports.changeStatus = async (req, res) => {
     const status = req.params.status;
     const id = req.params.id;
@@ -57,5 +57,13 @@ module.exports.changeMulti = async (req, res) => {
     if(type == "active")  await Product.updateMany({_id: {$in: ids}}, {status: "active"});
     else  await Product.updateMany({_id: {$in: ids}}, {status: "inactive"});
 
+    res.redirect("back");
+};
+
+
+//[DELETE] /admin/products/delete/:id
+module.exports.deleteItem = async (req, res) => {
+    const id = req.params.id;
+    await  Product.deleteOne({_id: id});
     res.redirect("back");
 };
--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -15,3 +15,21 @@ if(ButtonChangeStatus.length > 0){
         });
     });
 }
+
+const buttonsDelete = document.querySelectorAll("[button-delete");
+if(buttonsDelete){
+    const formDeleteItem = document.querySelector("#form-delete-item");
+    const path = formDeleteItem.getAttribute("data-path");
+
+    buttonsDelete.forEach(button => {
+        button.addEventListener("click", () => {
+            const isConfirm = confirm("Do you want delete this product?");
+            if(isConfirm){
+                const id = button.getAttribute("data-id");
+                const action = `${path}/${id}?_method=DELETE`;
+                formDeleteItem.action = action;
+                formDeleteItem.submit();
+            }
+        });
+    });
+}
